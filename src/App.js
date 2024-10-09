cbb80c5c18d76b904d6c76792d1bd96e1f9c751e
porperza <pikza_classic@hotmail.com>
2024-10-10 02:42:28 +0700

    src/App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import initializeApp from './app/init';
 // Importing pages
 const Layout = lazy(() => import('./containers/Layout'))
 const Login = lazy(() => import('./pages/Login'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
-const Register = lazy(() => import('./pages/Register'))
-const Documentation = lazy(() => import('./pages/Documentation'))
 
 
 // Initializing different libraries
@@ -34,14 +31,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/documentation" element={<Documentation />} />
           
           {/* Place new routes over this */}
           <Route path="/app/*" element={<Layout />} />
 
-          <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/>
+          <Route path="*" element={<Navigate to={token ? "/app/dashboard" : "/login"} replace />}/>
 
         </Routes>
       </Router>
